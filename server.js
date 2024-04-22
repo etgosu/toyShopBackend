6a1db1cc8df634a657072ca57c5eb0853f8cb206
kyn <rladudshka@naver.com>
2024-04-22 14:51:47 +0900

    axios요청에 응답코드에 상태코드를 추가하는방법

--- server.js
+++ server.js
@@ -1,8 +1,38 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+app.use(cors())
 
 app.get('/', function(req, res){
-    return res.send('main shopping mall')
+    // const resData =
+    // {
+    //     // `data` is the response that was provided by the server
+    //     data: {},
+
+    //     // `status` is the HTTP status code from the server response
+    //     status: 200,
+
+    //     // `statusText` is the HTTP status message from the server response
+    //     statusText: 'OK',
+
+    //     // `headers` the HTTP headers that the server responded with
+    //     // All header names are lowercase and can be accessed using the bracket notation.
+    //     // Example: `response.headers['content-type']`
+    //     headers: {},
+
+    //     // `config` is the config that was provided to `axios` for the request
+    //     config: {},
+
+    //     // `request` is the request that generated this response
+    //     // It is the last ClientRequest instance in node.js (in redirects)
+    //     // and an XMLHttpRequest instance in the browser
+    //     request: {}
+    // }
+    
+    const resData = 'Hello shop MainPage';
+    
+    return res.send(resData)
 })
 
 app.listen(3000, function(){
