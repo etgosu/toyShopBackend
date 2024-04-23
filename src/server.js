34b5066e98809090a149fe00a9f88a9d57885e7e
kyn <rladudshka@naver.com>
2024-04-24 07:49:20 +0900

    1.신규 API 구현:{ 상품리스트, 상품조회 } 2.DB연결 완료시 console 출력 메시지 변경

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 const mongoose = require('mongoose');
 const MONGO_URI = 'mongodb://localhost:27017/myshopdb';
-mongoose.connect(MONGO_URI).then(result => console.log({result}));
+mongoose.connect(MONGO_URI).then(result => console.log('DB connected'));
 
 const { Product } = require('../models/Product');
 
@@ -12,33 +12,8 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', function(req, res){
-    // const resData =
-    // {
-    //     // `data` is the response that was provided by the server
-    //     data: {},
 
-    //     // `status` is the HTTP status code from the server response
-    //     status: 200,
-
-    //     // `statusText` is the HTTP status message from the server response
-    //     statusText: 'OK',
-
-    //     // `headers` the HTTP headers that the server responded with
-    //     // All header names are lowercase and can be accessed using the bracket notation.
-    //     // Example: `response.headers['content-type']`
-    //     headers: {},
-
-    //     // `config` is the config that was provided to `axios` for the request
-    //     config: {},
-
-    //     // `request` is the request that generated this response
-    //     // It is the last ClientRequest instance in node.js (in redirects)
-    //     // and an XMLHttpRequest instance in the browser
-    //     request: {}
-    // }
-    
     const resData = 'Hello shop MainPage';
-    
     return res.send(resData)
 })
 
@@ -52,23 +27,24 @@ app.get('/login', function(req, res){
 
 app.post('/product', async (req, res) =>{
     try {
-            // console.log(req.body);
             const product = new Product(req.body);
             await product.save(); 
             return res.send({ product })
-        
     } catch (err) {
         console.log(err)
         return res.status(500).send({ err:err.message });
     }
 })
 
-app.get('/prodList', function(req, res){
-    return res.send('prod list')
+app.get('/prodList', async (req, res)=>{
+    const prodList = await Product.find();
+    return res.send({prodList});
 })
 
-app.get('/prodDetail', function(req, res){
-    return res.send('prod Detail')
+app.get('/prodDetail/:prodNm', async (req, res) =>{
+    const { prodNm } = req.params;
+    const prod = await Product.findOne({prodNm : prodNm})
+    return res.send({ prod })
 })
 
 app.get('/basketList', function(req, res){
