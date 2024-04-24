e84435faf3b3a652f18ce686328d005c019179cc
kyn <rladudshka@naver.com>
2024-04-25 01:41:05 +0900

    모델추가[유저,이벤트,장바구니]

---
+++ models/Event.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const EventSchema = new mongoose.Schema({
+    eventId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    eventName: {
+        type: String,
+        required: true
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    endDate: {
+        type: Date,
+        required: true
+    },
+    isHidden: {
+        type: Boolean,
+        default: false
+    },
+    eventProduct: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+    },
+    eventQuantity: {
+        type: Number,
+        default: 0
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    updatedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+}, { timestamps: true }); // timestamps 옵션을 true로 설정하여 생성일과 수정일을 자동으로 관리
+
+const Event = mongoose.model('Event', EventSchema);
+
+module.exports = { Event };
---
+++ models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    loginFailCount: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    isAccountLocked: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    lastLoginAt: Date,
+    dormancyConversionDate: Date
+}, { timestamps: true });
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = { User };
+
