f3df32952714f3dc0b2fff1b60b5f37c264040ea
kyn <rladudshka@naver.com>
2024-04-25 17:44:39 +0900

    [모델변경] 상품코드의 키값변경

--- models/Basket.js
+++ models/Basket.js
@@ -3,10 +3,10 @@ const { Schema } = mongoose;
 
 const BasketSchema = new mongoose.Schema({
     userId: { type: Schema.Types.ObjectId, ref: 'User', required: true }, // 장바구니 소유자ID
-    productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true }, // 상품모델ID
+    prodCd: { type: Schema.Types.ObjectId, ref: 'Product', required: true }, // 상품모델ID
     qty: { type: Number, required: true, min: 1 }, // 장바구니 제품수량
 }, { timestamps: true });
 
 const Basket = mongoose.model('Basket', BasketSchema);
 
-module.exports = { Basket };
+module.exports =  Basket ;
