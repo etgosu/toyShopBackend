7e5144517a8eab629d42d3d9c7d7dc77b3971f74
kyn <rladudshka@naver.com>
2024-05-08 00:05:24 +0900

    [virtual 필드를 추가함] 사용자, 상품 정보에대한 직관적인 KEY를 가지는 JSON형태에 데이터를 만들기 위함

--- models/Basket.js
+++ models/Basket.js
@@ -2,10 +2,30 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const BasketSchema = new mongoose.Schema({
-    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true }, // 장바구니 소유자ID
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true, unique : true }, // 장바구니 소유자ID
     prodCd: { type: Schema.Types.ObjectId, ref: 'Product', required: true }, // 상품모델ID
     qty: { type: Number, required: true, min: 1 }, // 장바구니 제품수량
-}, { timestamps: true });
+}, { timestamps: true }
+);
+
+BasketSchema.virtual('userInfo', {
+  ref: 'User',
+  localField: 'userId',
+  foreignField: '_id',
+  justOne: true,
+  options: { select: 'userNm' }
+});
+
+BasketSchema.virtual('prodInfo', {
+  ref: 'Product',
+  localField: 'prodCd',
+  foreignField: '_id',
+  justOne: true,
+  options: { select: 'prodNm price' }
+});
+
+BasketSchema.set('toObject', { virtuals: true });
+BasketSchema.set('toJSON', { virtuals: true });
 
 const Basket = mongoose.model('Basket', BasketSchema);
 
