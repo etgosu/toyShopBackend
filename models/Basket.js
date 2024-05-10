925ae88ebd0dbf187d9a9b90f0c7fd643045573e
kyn <rladudshka@naver.com>
2024-05-10 23:45:57 +0900

    장바구니에 여러 아이템이 올수 있기 때문에 userId에 Unique한 속성을 주지 않느것으로 변경

--- models/Basket.js
+++ models/Basket.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const BasketSchema = new mongoose.Schema({
-    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true, unique : true }, // 장바구니 소유자ID
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true }, // 장바구니 소유자ID
     prodCd: { type: Schema.Types.ObjectId, ref: 'Product', required: true }, // 상품모델ID
     qty: { type: Number, required: true, min: 1 }, // 장바구니 제품수량
 }, { timestamps: true }
